857f3516fe176a05ff2f6274119f29e8720a611f
Joao Lucas <joaolucasmbcadorniga@gmail.com>
2025-04-17 11:16:24 -0300

    FEAT: basic files

---
+++ test.py
@@ -0,0 +1,22 @@
+from DSSE import CoverageDroneSwarmSearch
+from stable_baselines3 import PPO
+
+env = CoverageDroneSwarmSearch(
+    drone_amount=3,
+    render_mode="human",
+    disaster_position=(-24.04, -46.17),  # (lat, long)
+    pre_render_time=10, # hours to simulate
+)
+
+opt = {
+    "drones_positions": [(0, 10), (10, 10), (20, 10)],
+}
+obs, info = env.reset(options=opt)
+
+step = 0
+while env.agents:
+    step += 1
+    actions = {agent: env.action_space(agent).sample() for agent in env.agents}
+    observations, rewards, terminations, truncations, infos = env.step(actions)
+
+print(infos["drone0"])
